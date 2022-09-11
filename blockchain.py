ec1228fc00bbfcaf8799ee02ce48b0ffd29a9a38
ndgndg91 <ndgndg91@gmail.com>
2022-09-11 21:29:32 +0900

    <refactor> formatting

--- blockchain.py
+++ blockchain.py
@@ -97,58 +97,56 @@ class Blockchain:
             return True
         return False
 
-# Par 2 - Mining our Blockchain
-
-# Creating a Web App
-app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
-
-# Creating a Blockchain
-blockchain = Blockchain()
-
-
-# Mining a new block
-@app.route('/mine_block', methods=['GET'])
-def mine_block():
-    previous_block = blockchain.get_previous_block()
-    previous_proof = previous_block['proof']
-    proof = proof_of_work(previous_proof=previous_proof)
-    previous_hash = create_hash(previous_block)
-    block = blockchain.create_block(proof, previous_hash)
-    response = {
-        'message': 'Congratulations, you just mined a block!',
-        'index': block['index'],
-        'timestamp': block['timestamp'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash']
-    }
-    return jsonify(response), 200
-
-
-# Getting the full Blockchain
-@app.route('/get_chain', methods=['GET'])
-def get_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain)
-    }
-    return jsonify(response), 200
-
-
-# Check Blockchain is valid
-@app.route('/is_valid', methods=['GET'])
-def is_valid_chain():
-    valid = is_chain_valid(blockchain.chain)
-    response = {
-        'valid': valid
-    }
-    return jsonify(response), 200
-
-# Part 3 - Decentralizing our Blockchain
-
 
 # Running the app
 if __name__ == '__main__':
+    # Par 2 - Mining our Blockchain
+
+    # Creating a Web App
+    app = Flask(__name__)
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+    # Creating a Blockchain
+    blockchain = Blockchain()
+
+    # Mining a new block
+    @app.route('/mine_block', methods=['GET'])
+    def mine_block():
+        previous_block = blockchain.get_previous_block()
+        previous_proof = previous_block['proof']
+        proof = proof_of_work(previous_proof=previous_proof)
+        previous_hash = create_hash(previous_block)
+        block = blockchain.create_block(proof, previous_hash)
+        response = {
+            'message': 'Congratulations, you just mined a block!',
+            'index': block['index'],
+            'timestamp': block['timestamp'],
+            'proof': block['proof'],
+            'previous_hash': block['previous_hash']
+        }
+        return jsonify(response), 200
+
+    # Getting the full Blockchain
+    @app.route('/get_chain', methods=['GET'])
+    def get_chain():
+        response = {
+            'chain': blockchain.chain,
+            'length': len(blockchain.chain)
+        }
+        return jsonify(response), 200
+
+    # Check Blockchain is valid
+    @app.route('/is_valid', methods=['GET'])
+    def is_valid_chain():
+        valid = is_chain_valid(blockchain.chain)
+        response = {
+            'valid': valid
+        }
+        return jsonify(response), 200
+
+
+    # Part 3 - Decentralizing our Blockchain
+
     app.run(
         host='0.0.0.0',
         port=8080
